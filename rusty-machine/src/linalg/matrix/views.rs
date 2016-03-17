8f50941862abf87d427bb8d077892f24359c2e56
James Lucas <LucasJ94@hotmail.co.uk>
2016-03-16 21:40:39 -0700

    Documentation and unit tests

--- rusty-machine/src/linalg/matrix/views.rs
+++ rusty-machine/src/linalg/matrix/views.rs
@@ -3,111 +3,128 @@ use super::Matrix;
 use std::ops::{Mul, Add, Div, Sub, Index, Neg};
 use libnum::{One, Zero};
 
+/// A MatrixView
+///
+/// This struct provides a view into a matrix.
+///
+/// The struct contains the upper left point of the view
+/// and the width and height of the view.
 #[derive(Debug, Clone, Copy)]
 pub struct MatrixView<'a, T: 'a> {
-	window_corner: [usize; 2],
-	rows: usize,
-	cols: usize,
-	mat: &'a Matrix<T>,
+    window_corner: [usize; 2],
+    rows: usize,
+    cols: usize,
+    mat: &'a Matrix<T>,
 }
 
 impl<'a, T> MatrixView<'a, T> {
+	/// Return the number of rows in the view.
+    pub fn rows(&self) -> usize {
+        self.rows
+    }
 
-	pub fn rows(&self) -> usize {
-		self.rows
-	}
+    /// Return the number of columns in the view.
+    pub fn cols(&self) -> usize {
+        self.cols
+    }
 
-	pub fn cols(&self) -> usize {
-		self.cols
-	}
+    /// Produce a matrix view from a matrix
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use rusty_machine::linalg::matrix::Matrix;
+    /// use rusty_machine::linalg::matrix::views::MatrixView;
+    ///
+    /// let a = Matrix::new(3,3, (0..9).collect::<Vec<usize>>());
+    /// let view = MatrixView::from_matrix(&a, [1,1], 2, 2);
+    /// ```
+    pub fn from_matrix(mat: &'a Matrix<T>,
+                       start: [usize; 2],
+                       rows: usize,
+                       cols: usize)
+                       -> MatrixView<'a, T> {
+        assert!(start[0] + rows <= mat.rows(),
+                "View dimensions exceed matrix dimensions.");
+        assert!(start[1] + cols <= mat.cols(),
+                "View dimensions exceed matrix dimensions.");
 
-	/// Produce a matrix view from a matrix
-	///
-	/// # Examples
-	///
-	/// ```
-	/// use rusty_machine::linalg::matrix::Matrix;
-	/// use rusty_machine::linalg::matrix::views::MatrixView;
-	///
-	/// let a = Matrix::new(3,3, (0..9).collect::<Vec<usize>>());
-	/// let view = MatrixView::from_matrix(&a, [1,1], 2, 2);
-	/// ```
-	pub fn from_matrix(mat: &'a Matrix<T>, start: [usize;2], rows: usize, cols: usize) -> MatrixView<'a, T> {
-		assert!(start[0] + rows <= mat.rows(), "View dimensions exceed matrix dimensions.");
-		assert!(start[1] + cols <= mat.cols(), "View dimensions exceed matrix dimensions.");
+        MatrixView {
+            window_corner: start,
+            rows: rows,
+            cols: cols,
+            mat: mat,
+        }
+    }
 
-		MatrixView {
-			window_corner: start,
-			rows: rows,
-			cols: cols,
-			mat: mat,
-		}
-	}
-
-	/// Returns an iterator over the matrix view.
-	///
-	/// # Examples
-	///
-	/// ```
-	/// use rusty_machine::linalg::matrix::Matrix;
-	/// use rusty_machine::linalg::matrix::views::MatrixView;
-	///
-	/// let a = Matrix::new(3,3, (0..9).collect::<Vec<usize>>());
-	/// let view = MatrixView::from_matrix(&a, [1,1], 2, 2);
-	///
-	/// let view_data = view.iter().map(|v| *v).collect::<Vec<usize>>();
-	/// assert_eq!(view_data, vec![4,5,7,8]);
-	/// ```
-	pub fn iter(&'a self) -> ViewIter<'a, T> {
-		ViewIter {
-			view: self,
-			row_pos: 0,
-			col_pos: 0,
-		}
-	}
+    /// Returns an iterator over the matrix view.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use rusty_machine::linalg::matrix::Matrix;
+    /// use rusty_machine::linalg::matrix::views::MatrixView;
+    ///
+    /// let a = Matrix::new(3,3, (0..9).collect::<Vec<usize>>());
+    /// let view = MatrixView::from_matrix(&a, [1,1], 2, 2);
+    ///
+    /// let view_data = view.iter().map(|v| *v).collect::<Vec<usize>>();
+    /// assert_eq!(view_data, vec![4,5,7,8]);
+    /// ```
+    pub fn iter(&'a self) -> ViewIter<'a, T> {
+        ViewIter {
+            view: self,
+            row_pos: 0,
+            col_pos: 0,
+        }
+    }
 }
 
 impl<'a, T: Copy> MatrixView<'a, T> {
-
-	pub fn into_matrix(self) -> Matrix<T> {
-		let view_data = self.iter().map(|v| *v).collect::<Vec<T>>();
-		Matrix {
-			rows: self.rows,
-			cols: self.cols,
-			data: view_data,
-		}
-	}
+	/// Convert the matrix view into a new Matrix.
+    pub fn into_matrix(self) -> Matrix<T> {
+        let view_data = self.iter().map(|v| *v).collect::<Vec<T>>();
+        Matrix {
+            rows: self.rows,
+            cols: self.cols,
+            data: view_data,
+        }
+    }
 }
 
+/// Iterator for the MatrixView
+///
+/// Iterates over the underlying view data
+/// in row-major order.
 #[derive(Debug)]
 pub struct ViewIter<'a, T: 'a> {
-	view: &'a MatrixView<'a, T>,
-	row_pos: usize,
-	col_pos: usize,
+    view: &'a MatrixView<'a, T>,
+    row_pos: usize,
+    col_pos: usize,
 }
 
 impl<'a, T> Iterator for ViewIter<'a, T> {
-	type Item = &'a T;
+    type Item = &'a T;
 
-	fn next(&mut self) -> Option<Self::Item> {
-		let raw_row_idx = self.view.window_corner[0] + self.row_pos;
-		let raw_col_idx = self.view.window_corner[1] + self.col_pos;
+    fn next(&mut self) -> Option<Self::Item> {
+        let raw_row_idx = self.view.window_corner[0] + self.row_pos;
+        let raw_col_idx = self.view.window_corner[1] + self.col_pos;
 
-		// Set the position of the next element
-		if self.row_pos < self.view.rows {
-			// If end of row, set to start of next row
-			if self.col_pos == self.view.cols - 1 {
-				self.row_pos += 1usize;
-				self.col_pos = 0usize;
-			} else {
-				self.col_pos += 1usize;
-			}
+        // Set the position of the next element
+        if self.row_pos < self.view.rows {
+            // If end of row, set to start of next row
+            if self.col_pos == self.view.cols - 1 {
+                self.row_pos += 1usize;
+                self.col_pos = 0usize;
+            } else {
+                self.col_pos += 1usize;
+            }
 
-			Some(&self.view.mat[[raw_row_idx, raw_col_idx]])
-		} else {
-			None
-		}
-	}
+            Some(&self.view.mat[[raw_row_idx, raw_col_idx]])
+        } else {
+            None
+        }
+    }
 }
 
 /// Indexes matrix.
@@ -121,7 +138,12 @@ impl<'a, T> Index<[usize; 2]> for MatrixView<'a, T> {
                 "Row index is greater than row dimension.");
         assert!(idx[1] < self.cols,
                 "Column index is greater than column dimension.");
-        unsafe { &self.mat.data().get_unchecked((self.window_corner[0] + idx[0]) * self.cols + self.window_corner[1] + idx[1]) }
+
+        unsafe {
+            &self.mat.data().get_unchecked((self.window_corner[0] + idx[0]) * self.mat.cols +
+                                           self.window_corner[1] +
+                                           idx[1])
+        }
     }
 }
 
@@ -210,9 +232,7 @@ impl<'a, 'b, 'c, T: Copy + Zero + One + Mul<T, Output=T> + Add<T, Output=T>> Mul
                 {
                     for j in 0..m.cols
                     {
-                        new_data[i*m.cols() + j] = *new_data.get_unchecked(i*m.cols() + j) +
-                        							*self.mat.data().get_unchecked((self.window_corner[0] + i) * self.cols + (self.window_corner[1] + k)) *
-                        							*m.data().get_unchecked(k*m.cols + j);
+                        new_data[i*m.cols() + j] = *new_data.get_unchecked(i*m.cols() + j) + *self.mat.data().get_unchecked((self.window_corner[0] + i) *self.cols + (self.window_corner[1] + k)) * *m.data().get_unchecked(k*m.cols + j);
                     }
                 }
             }
@@ -558,7 +578,7 @@ impl<'a, T: Copy + One + Zero + PartialEq + Div<T, Output = T>> Div<T> for Matri
 }
 
 /// Divides matrix by scalar.
-impl<'a,'b, T: Copy + One + Zero + PartialEq + Div<T, Output = T>> Div<T> for &'b MatrixView<'a, T> {
+impl<'a, 'b, T: Copy + One + Zero + PartialEq + Div<T, Output = T>> Div<T> for &'b MatrixView<'a, T> {
     type Output = Matrix<T>;
 
     fn div(self, f: T) -> Matrix<T> {
@@ -620,4 +640,105 @@ impl<'a, 'b, T: Neg<Output = T> + Copy> Neg for &'b MatrixView<'a, T> {
             data: new_data,
         }
     }
+}
+
+#[cfg(test)]
+mod tests {
+	use super::MatrixView;
+	use super::super::Matrix;
+
+	#[test]
+	#[should_panic]
+	fn make_view() {
+		let a = Matrix::new(3,3, vec![2.0; 9]);
+		let _ = MatrixView::from_matrix(&a, [1,1], 3, 2);
+	}
+
+	#[test]
+	fn add_view() {
+		let a = 3.0;
+		let b = Matrix::new(3,3, vec![2.0; 9]);
+		let c = Matrix::new(2,2, vec![1.0; 4]);
+
+		let d = MatrixView::from_matrix(&b, [1,1], 2, 2);
+
+		let m_1 = &d + a;
+		assert_eq!(m_1.into_vec(), vec![5.0; 4]);
+
+		let m_2 = &d + c;
+		assert_eq!(m_2.into_vec(), vec![3.0; 4]);
+
+		let m_3 = &d + &d;
+		assert_eq!(m_3.into_vec(), vec![4.0; 4]);		
+	}
+
+	#[test]
+	fn sub_view() {
+		let a = 3.0;
+		let b = Matrix::new(2,2, vec![1.0; 4]);
+		let c = Matrix::new(3,3, vec![2.0; 9]);
+
+		let d = MatrixView::from_matrix(&c, [1,1], 2, 2);
+
+		let m_1 = &d - a;
+		assert_eq!(m_1.into_vec(), vec![-1.0; 4]);
+
+		let m_2 = &d - b;
+		assert_eq!(m_2.into_vec(), vec![1.0; 4]);
+
+		let m_3 = &d - &d;
+		assert_eq!(m_3.into_vec(), vec![0.0; 4]);
+	}
+
+	#[test]
+	fn mul_view() {
+		let a = 3.0;
+		let b = Matrix::new(2,2, vec![1.0; 4]);
+		let c = Matrix::new(3,3, vec![2.0; 9]);
+
+		let d= MatrixView::from_matrix(&c, [1,1], 2, 2);
+
+		let m_1 = &d * a;
+		assert_eq!(m_1.into_vec(), vec![6.0; 4]);
+
+		let m_2 = &d * b;
+		assert_eq!(m_2.into_vec(), vec![4.0; 4]);
+
+		let m_3 = &d * d;
+		assert_eq!(m_3.into_vec(), vec![8.0; 4]);
+	}
+
+	#[test]
+	fn div_view() {
+		let a = 3.0;
+
+		let b = Matrix::new(3,3, vec![2.0; 9]);
+
+		let c = MatrixView::from_matrix(&b, [1,1], 2, 2);
+
+		let m = c / a;
+		assert_eq!(m.into_vec(), vec![2.0/3.0 ;4]);
+	}
+
+	#[test]
+	fn neg_view() {
+		let b = Matrix::new(3,3, vec![2.0; 9]);
+
+		let c = MatrixView::from_matrix(&b, [1,1], 2, 2);
+
+		let m = -c;
+		assert_eq!(m.into_vec(), vec![-2.0;4]);
+	}
+
+	#[test]
+	fn index_view() {
+		let b = Matrix::new(3,3, (0..9).collect());
+
+		let c = MatrixView::from_matrix(&b, [1,1], 2, 2);
+		
+		assert_eq!(c[[0,0]], 4);
+		assert_eq!(c[[0,1]], 5);
+		assert_eq!(c[[1,0]], 7);
+		assert_eq!(c[[1,1]], 8);
+	}
 }
